1b3e70aad92f1674a5aed417894bd77da9cfdc6b
SUP'Internet 08 <supinternet08@MacBook-Air-de-SUPInternet-41.local>
2017-09-21 11:28:19 +0200

    to do : BCRYPT + argv

--- hashid.php
+++ hashid.php
@@ -1,7 +1,7 @@
 <?php
+	
 
-	$list_algo = hash_algos();
-	foreach($list_algo as $algo){
-	echo($algo);
-		}
-	echo($list_algo);
+	//Trouver bcrypt avec son salt (utiliser la methode de php en trouvant les 2 $salt$password) => + faire le system de parametre (argc avec en param sur le CLI le hash puis le parse avec le tableau)
+	$list_algo = ["CRC16" => 6, "Adler32" => 8, "md5" => 32, "sha1" => 40, "bcrypt" => "todo", "whirlpool" => 128];
+
+        echo(strlen("e19423c475bde38dba1e985ab8126682473307e166b65719f11a2aac423214a1cbabb7f777b2a8d3c4611ceec33739c0bde2a062b5e39cd937e2cb03c8265e98"));
