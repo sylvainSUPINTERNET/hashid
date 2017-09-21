3f750eda1c4bfe03cacad132f1b0a982db08a561
SUP'Internet 08 <supinternet08@MacBook-Air-de-SUPInternet-41.local>
2017-09-21 12:15:17 +0200

    to do : BCRYPT + 15 autre algo et rrevoir le system derreur + arguments

--- hashid.php
+++ hashid.php
@@ -1,7 +1,25 @@
 <?php
-	
 
-	//Trouver bcrypt avec son salt (utiliser la methode de php en trouvant les 2 $salt$password) => + faire le system de parametre (argc avec en param sur le CLI le hash puis le parse avec le tableau)
-	$list_algo = ["CRC16" => 6, "Adler32" => 8, "md5" => 32, "sha1" => 40, "bcrypt" => "todo", "whirlpool" => 128];
+        //Trouver bcrypt avec son salt (utiliser la methode de php en trouvant les 2 $salt$password) => + faire le system de parametre (argc avec en param sur le CLI le hash puis le parse avec le tableau)
+        $list_algo = [6 => "CRC16", 8 => "Adler32", 32 => "md5", 40 => "sha1", "bcrypt" => "todo", 128 => "whirlpool"];
 
-        echo(strlen("e19423c475bde38dba1e985ab8126682473307e166b65719f11a2aac423214a1cbabb7f777b2a8d3c4611ceec33739c0bde2a062b5e39cd937e2cb03c8265e98"));
+	if($argv[1] && is_string($argv[1])){
+		$hash_length = strlen($argv[1]);	
+		print "". $list_algo[$hash_length];
+		
+		//if find nothing 
+		if($list_algo[$hash_length] == ""){
+			EOL;
+			// try another list of 15 
+			// if still nothing we return no algo find for this hash 
+			print " No algorithm found for this hash : " . $argv[1];
+		}
+	}else{
+
+	EOL;
+	print " Please enter a hash into paramters ... ";
+	}
+
+
+     
+
